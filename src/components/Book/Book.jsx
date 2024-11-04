d06e7b8d2ac265e4e8cdb6435c924bc548ba0236
Sreedhar-roy <sreedharroy0@gmail.com>
2024-11-05 00:49:39 +0600

    card section done

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,6 +1,6 @@
 
 const Book = ({book}) => {
-    const {image, bookName} = book;
+    const {image, bookName, author, publisher} = book;
     return (
         <div className="card bg-base-100 mb-8 w-96 shadow-xl">
             <figure className=" bg-blue-200 py-8 ">
@@ -14,7 +14,7 @@ const Book = ({book}) => {
                     {bookName}
                     <div className="badge badge-secondary">NEW</div>
                 </h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <p>{author}</p>
                 <div className="card-actions justify-end">
                     <div className="badge badge-outline">Fashion</div>
                     <div className="badge badge-outline">Products</div>
