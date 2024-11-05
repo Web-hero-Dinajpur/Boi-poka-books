27ff3f24b4b1d976df835d654de8a8c2a41d0552
Sreedhar-roy <sreedharroy0@gmail.com>
2024-11-05 10:11:24 +0600

    card rating and category done

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,6 +1,6 @@
 
-const Book = ({book}) => {
-    const {image, bookName, author, publisher} = book;
+const Book = ({ book }) => {
+    const { image, bookName, author, publisher, rating, category, tags } = book;
     return (
         <div className="card bg-base-100 mb-8 w-96 shadow-xl">
             <figure className=" bg-blue-200 py-8 ">
@@ -10,14 +10,27 @@ const Book = ({book}) => {
                     alt={bookName} />
             </figure>
             <div className="card-body">
+                <div className="flex gap-4 ">
+                    {
+                        tags.map(tag => <button className="btn btn-xs text-green-600">{tag}</button>)
+                    }
+                </div>
                 <h2 className="card-title">
                     {bookName}
-                    <div className="badge badge-secondary">NEW</div>
                 </h2>
-                <p>{author}</p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div>
-                    <div className="badge badge-outline">Products</div>
+                <p>By : {author}</p>
+                <hr />
+                <div className="card-actions justify-between mt-4 items-center">
+                    <div className="badge badge-outline text-green-600 font-semibold">{category}</div>
+                    <div>
+                        <div className="rating">
+                            <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+                            <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" defaultChecked />
+                            <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+                            <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-400" />
+                            <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-200" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
